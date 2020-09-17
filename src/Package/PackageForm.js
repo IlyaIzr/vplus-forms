0df212fb6bfc19b7227a2325c0372c7ad5070e3a
ilyaizr <ilyaizrailyan@gmail.com>
2020-09-17 17:39:29 +0300

    multiple select via react-select library

--- src/Package/PackageForm.js
+++ src/Package/PackageForm.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated';
+//Fake data
+import { timeout, roomsFieldMeta } from './fakeData';
+// Extra stuff
+const animatedComponents = makeAnimated();
 
 export const PackageForm = () => {
+
+  const [roomsField, setRoomsField] = useState({
+    options: [],
+    isEditable: false,
+    isRequired: true,
+    value: []
+  })
+  const onRoomsChange = options => {
+    setRoomsField({ ...roomsField, value: options })
+  }
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      const options = []
+      roomsFieldMeta.options.forEach((serverOption) => {
+        options.push({ value: serverOption.id, label: serverOption.name })
+      })
+      const value = []
+      roomsFieldMeta.value.forEach((serverValue) => {
+        value.push({ value: serverValue.id, label: serverValue.name })
+      })
+      setRoomsField({ ...roomsFieldMeta, options, value })
+    }, timeout);
+  }, [])
+
   return (
-    <div>
-    PackageForm
+    <div className="ui container">
+      <br />
+      <form className="ui from">
+        <div className="field">
+          <label htmlFor="rooms">Выберите один или несколько румов</label>
+          <Select name="rooms" options={roomsField.options} isMulti onChange={onRoomsChange}
+            components={animatedComponents} value={roomsField.value}
+          />
+        </div>
+      </form>
     </div>
   )
 }
---
+++ src/Package/fakeData.js
@@ -0,0 +1,12 @@
+export const roomsFieldMeta = {
+  options: [
+    { name: 'redroom', id: 142 },
+    { name: 'blueroom', id: 122 },
+    { name: 'grayroom', id: 152 },
+  ],
+  isEditable: true,
+  isRequired: true,
+  value: [{ name: 'redroom', id: 142 }]
+}
+
+export const timeout = 500
