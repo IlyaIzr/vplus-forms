a9abe7c30c2bcdeab2a09e61ddd2480506983daf
ilyaizr <ilyaizrailyan@gmail.com>
2020-09-28 15:57:40 +0300

    pushing to build folder

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     accountsForm: path.join(__dirname, './src/Accs/index.js')
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: '[name].js',
     chunkFilename: '[name].common.js',
   },
