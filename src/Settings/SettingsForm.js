d1e6bbc3bb528b00809767544a5328a363f851ac
ilyaizr <ilyaizrailyan@gmail.com>
2020-10-08 17:44:43 +0300

    settingForm change password no server hook yet

--- src/Settings/SettingsForm.js
+++ src/Settings/SettingsForm.js
@@ -10,6 +10,35 @@ export const SettingsForm = () => {
   const [eMailField, setEMailField] = useState('')
   const [skypeField, setSkypeField] = useState(stringFieldDefaultState)
   const onSkypeChange = e => setSkypeField({ ...skypeField, value: e.target.value })
+  //Tabs
+  const [activeTab, setActiveTab] = useState(null)
+  const onTabClick = e => setActiveTab(e.target.name)
+  //Tab 1, passwords
+  const [oldPWord, setOldPWord] = useState('')
+  const onOldPWord = e => setOldPWord(e.target.value)
+  const [newPWord, setNewPWord] = useState('')
+  const onNewPWord = e => setNewPWord(e.target.value)
+  const [newPWord2, setNewPWord2] = useState('')
+  const onNewPWord2 = e => setNewPWord2(e.target.value)
+  const [pWordStatus, setPWordStatus] = useState(null)
+  const submitPWord = e => {
+    e.preventDefault()
+    e.target.checkValidity()
+    const data = {
+      fund: fundField,
+      email: eMailField,
+      oldPassword: oldPWord,
+      newPassword: newPWord
+    }
+    console.log(data)
+    resetPWords()
+    setPWordStatus('Success')
+  }
+  const resetPWords = () => {
+    setOldPWord('')
+    setNewPWord('')
+    setNewPWord2('')
+  }
 
   useEffect(() => {
     setFundField(fundFieldMeta)
@@ -33,10 +62,67 @@ export const SettingsForm = () => {
 
         <StringField label="Skype" name="skype"
           isEditable={skypeField.isEditable} isRequired={skypeField.isRequired}
-          value={skypeField.value} onChange={onSkypeChange} 
+          value={skypeField.value} onChange={onSkypeChange}
         />
 
+        {/* Tab control */}
+        <div className="inline">
+          <button className="ui button small teal" type="button" onClick={onTabClick} name="1">
+            @(Изменить пароль)
+            </button>
+          <button className="ui button small teal" type="button" onClick={onTabClick} name="2">
+            @(Редактировать сотрудников)
+            </button>
+          <button className="ui button small teal" type="button" onClick={onTabClick} name="3">
+            @(Добавить сотрудника)
+            </button>
+        </div>
+
       </form>
+      {/* Active tab */}
+      {activeTab == 1 && <form className="ui form" onSubmit={submitPWord} onReset={resetPWords}>
+
+        <StringField name="password" label="@(Старый пароль)" type="password"
+          isEditable={true} isRequired={true}
+          value={oldPWord} onChange={onOldPWord}
+        />
+
+        <StringField name="password" label="@(Новый пароль)" type="password"
+          isEditable={true} isRequired={true}
+          value={newPWord} onChange={onNewPWord}
+        />
+        <StringField name="password" label="@(Повторите пароль)" type="password"
+          isEditable={true} isRequired={true}
+          value={newPWord2} onChange={onNewPWord2}
+        />
+
+        <div className="field inline disabled">
+          <label htmlFor="pwordCheck">@(Пароли совпадают)</label>
+          <input type="checkbox" required name="pwordCheck" className=""
+            checked={newPWord === newPWord2 && newPWord} />
+        </div>
+        {pWordStatus && <div className="ui alert message">
+          <h5 className="text red">{pWordStatus}</h5>
+        </div>}
+
+        <button className="ui button green small" type="submit">
+          @(Подтвредить)
+        </button>
+
+        <button className="ui button red small" type="reset">
+          @(Сбросить)
+        </button>
+
+      </form>}
+
+      {activeTab == 2 && <div className="ui segment">
+        peps2
+        </div>}
+
+      {activeTab == 3 && <div className="ui segment">
+        peps3
+        </div>}
+
     </div>
   )
 }
